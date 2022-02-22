8c234bfe8154c0a25b6fcfdcf240046f6db30ba2
William Li <email@williamli.dev>
2022-02-22 13:00:06 +0800

    add rel="noopener noreferrer" to links with _blank

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -26,7 +26,7 @@ function Home({now}) {
         </p>
 
         <div style={{textAlign: 'left', width: '100%'}}>
-          <a href="https://nextjs-time-api.vercel-support.app/api/time" target="_blank" style={{'textDecoration': 'underline'}}>Time API</a> Returned:
+          <a href="https://nextjs-time-api.vercel-support.app/api/time" target="_blank" rel="noopener noreferrer" style={{'textDecoration': 'underline'}}>Time API</a> Returned:
           <pre>
             {
               now
@@ -35,7 +35,7 @@ function Home({now}) {
         </div>
 
         <div style={{textAlign: 'left', width: '100%'}}>
-          <a href="https://nextjs-time-api.vercel-support.app/api/time" target="_blank" style={{'textDecoration': 'underline'}}>Time Cached API</a> Returned:
+          <a href="https://nextjs-time-api.vercel-support.app/api/time" target="_blank" rel="noopener noreferrer" style={{'textDecoration': 'underline'}}>Time Cached API</a> Returned:
           <pre>
             {
               nowCached
