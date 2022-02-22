5573a81788bd7fae07ca082105e6c3f1ee684f29
William Li <email@williamli.dev>
2022-02-22 13:04:40 +0800

    added some more debug logic

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 import moment from 'moment'
 import { useRouter } from 'next/router'
 
-function Home({now}) {
+function Home({now, nowCached}) {
   
   const router = useRouter()
   const { slug } = router.query
@@ -21,27 +21,14 @@ function Home({now}) {
         </h1>
 
         <p className={styles.description}>
-          Generated <code className={styles.code}>{router.asPath}</code> at {' '}
+          Timestamp from /api/time generated <code className={styles.code}>{router.asPath}</code> at {' '}
           <code className={styles.code}>{moment(now).format()} <span style={{fontSize: 'xx-small'}}>({moment(now).fromNow()} [{(Date.now() - now) / 1000} seconds])</span></code>.
         </p>
+        <p className={styles.description}>
+          Timestamp from /api/time-with-cache generated <code className={styles.code}>{router.asPath}</code> at {' '}
+          <code className={styles.code}>{moment(nowCached).format()} <span style={{fontSize: 'xx-small'}}>({moment(nowCached).fromNow()} [{(Date.now() - nowCached) / 1000} seconds])</span></code>.
+        </p>
 
-        <div style={{textAlign: 'left', width: '100%'}}>
-          <a href="https://nextjs-time-api.vercel-support.app/api/time" target="_blank" rel="noopener noreferrer" style={{'textDecoration': 'underline'}}>Time API</a> Returned:
-          <pre>
-            {
-              now
-            }
-          </pre>
-        </div>
-
-        <div style={{textAlign: 'left', width: '100%'}}>
-          <a href="https://nextjs-time-api.vercel-support.app/api/time" target="_blank" rel="noopener noreferrer" style={{'textDecoration': 'underline'}}>Time Cached API</a> Returned:
-          <pre>
-            {
-              nowCached
-            }
-          </pre>
-        </div>
 
         <div style={{textAlign: 'left', width: '100%'}}>
         router.isFallback:
@@ -72,16 +59,6 @@ function Home({now}) {
 
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
